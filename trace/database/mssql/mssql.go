59e57a7068876818960a99d298009ba611601cd5
scnace <scbizu@gmail.com>
2018-12-29 13:30:53 +0800

    database/mysql: add mysql tracer wraperr directly

--- trace/database/mssql/mssql.go
+++ trace/database/mssql/mssql.go
@@ -1,9 +1,16 @@
 package mssql
 
 import (
+	"github.com/ezbuy/wrapper"
 	"github.com/ezbuy/wrapper/trace/database"
 )
 
-func NewMsSQLTracer(ins string, user string) *database.Tracer {
+func NewMsSQLTracerWithMoreInfo(ins string, user string) *database.Tracer {
 	return database.NewCustmizedTracer(ins, user, "mssql")
+}
+
+func NewMsSQLTracerWrapper() wrapper.Wrapper {
+	return database.NewCustmizedTracerWrapper(
+		database.NewCustmizedTracer("", "", "mssql"), false,
+	)
 }
